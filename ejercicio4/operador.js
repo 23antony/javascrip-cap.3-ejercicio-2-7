a7e39aab34d92204d0adb37cd329dfa4190e1a5c
amarusadrian <amarusadrian@gmail.com>
2018-03-12 21:43:10 -0600

    Ejercicios 2 -5 completados

---
+++ ejercicio4/operador.js
@@ -0,0 +1,38 @@
+var valores = [true, 5, false, "hola", "adios", 2];
+
+//determina que cadena Es mayor y es "hola"
+resultado1 = valores[3] > valores[4];
+alert(resultado1);
+
+//combinar valores booleanos
+var var1 = valores[0];
+var var2 = valores[2];
+
+//obtener resultado FALSE
+resultado = var1 && var2; 
+alert(resultado);
+
+//obtener resultado TRUE
+var resultado = var1 || var2;
+alert(resultado);
+
+//operaciones matematicas 
+var num1 = valores[1];
+var num2 = valores[5];
+
+//operaciones Matematicas..
+suma = num1 + num2;
+alert(suma);
+
+resta = num1-num2;
+alert(resta);
+
+producto = num1*num2;
+alert(producto);
+
+div = num1/num2;
+alert(div);
+
+resto = num1%num2;
+alert(resto); 
+ 
